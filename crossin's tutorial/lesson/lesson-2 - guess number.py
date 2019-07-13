62035c55f4d067a6334c383d2a176fdba62e7b5d
KKKris <824246091@qq.com>
2019-07-13 13:49:18 +0800

    lesson-3

--- crossin's tutorial/lesson/lesson-2 - guess number.py
+++ crossin's tutorial/lesson/lesson-2 - guess number.py
@@ -8,10 +8,10 @@ while some == False:
     count += 1
     answer = int(input())
     if answer > num:
-        print('%d is too big!'% answer)
+        print('%d is too big!'% answer) #用 %d 字符化整数，%f 字符化为小数，%.2f 保留两位小数
     if answer < num:
         print('%d is too small'% answer)
     if answer == num:
-        print('bingo! %d is the right answer.'% answer)
+        print(f'bingo! {answer} is the right answer.') #用 f-string 把变量加入到输出里
         some = True
 print('你一共猜了',count,'次')
---
+++ crossin's tutorial/lesson/lesson-3.py
@@ -0,0 +1,11 @@
+for i in range(0,5):
+    for j in range(0,5):
+        print('*',end=' ')
+    print()
+
+print()
+
+for i in range(0,5):
+    for j in range(0,i+1):
+        print('*',end=' ')
+    print()

2486cc49151831973299e65e64e396f5a103d962
KKKris <824246091@qq.com>
2019-07-13 14:04:00 +0800

    Update lesson-2 - guess number.py

--- crossin's tutorial/lesson/lesson-2 - guess number.py
+++ crossin's tutorial/lesson/lesson-2 - guess number.py
@@ -8,7 +8,7 @@ while some == False:
     count += 1
     answer = int(input())
     if answer > num:
-        print('%d is too big!'% answer) #用 %d 字符化整数，%f 字符化为小数，%.2f 保留两位小数
+        print('%d is too big!'% answer) #用 %d 字符化整数，%s 为字符，%f 字符化为小数，%.2f 保留两位小数
     if answer < num:
         print('%d is too small'% answer)
     if answer == num:

ab209ec576b75079bf45da2aaafb617d5d9a13a1
KKKris <824246091@qq.com>
2019-07-31 23:25:57 +0800

    fill

--- Crawler/try-01.py
+++ Crawler/try-01.py
@@ -1,22 +1,45 @@
-import json, requests
+import json, requests, time
 from bs4 import BeautifulSoup
+
+# 保存已经抓取和未抓取的链接
+
+visited_urls = []
+unvisited_urls = ["https://www.chainnews.com/"]
+
+# 从队列中访问一个未抓取的链接
+
+def get_unvisited_urls():
+    while True:
+        if len(unvisited_urls) == 0:
+            return None
+        
+        url = unvisited_urls.pop()
+
+        if url in visited_urls:
+            continue
+        
+        visited_urls.append(url)
+        return url
 
 if __name__ == "__main__":
 
-    response = requests.get("https://www.chainnews.com/")
-    content = response.content.decode('utf-8')
-    #print(content)
+    while True:
+        url = get_unvisited_urls()
+        if url == None:
+            break
+        
+        response = requests.get(url)
+        content = response.content.decode('utf-8')
+        #print(content)
 
-    soup = BeautifulSoup(content,"html.parser")
-    # 获取当前页面的所有链接
-    
-    for element in soup.select("a"):
-        if not element.has_attr("href"):
-            continue
-        if not element["href"].startswith("https://"):
-            continue
-        print(element["href"])
-    
-    # 获取更多数据
-
-    
+        soup = BeautifulSoup(content,"html.parser")
+        # 获取当前页面的所有链接
+        
+        for element in soup.select("a"):
+            if not element.has_attr("href"):
+                continue
+            if not element["href"].startswith("https://"):
+                continue
+            print(element["href"])
+        
+        time.sleep(1)
